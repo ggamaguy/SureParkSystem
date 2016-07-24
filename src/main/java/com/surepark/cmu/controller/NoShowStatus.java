a795a0fdffb5daffe509f4d7462988fe773d113d
ggamaguy <ggamaguy@ajou.ac.kr>
2016-07-24 17:55:33 -0400

    no message

--- src/main/java/com/surepark/cmu/controller/NoShowStatus.java
+++ src/main/java/com/surepark/cmu/controller/NoShowStatus.java
@@ -1,10 +1,18 @@
 package com.surepark.cmu.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.surepark.cmu.interfaces.ReservationInterface;
 
 /**
  * Servlet implementation class NoShowStatus
@@ -12,6 +20,10 @@ import javax.servlet.http.HttpServletResponse;
 public class NoShowStatus extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	@Autowired
+	ReservationInterface reservationFacade;
+	@Autowired
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -19,7 +31,13 @@ public class NoShowStatus extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
+    @RequestMapping(value="/noshow/{phoneNumber}", 
+    		method = RequestMethod.POST,
+    		consumes="application/json")
+    public String deliveredNoshowStatus(@PathVariable(value="phoneNumber") String reservationId){
+    	
+    	return null;
+    }
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
